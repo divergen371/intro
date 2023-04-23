9dbfde9c78b00ea2a5615788e09d64354f15d071
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-04-23 18:14:40 +0900

    マップとキーワードリストについて

---
+++ app/v01/ch15/keyword_lists2.exs
@@ -0,0 +1,4 @@
+options = [foo: 1, bar: 2]
+options = [{:spam, 0} | options]
+
+IO.inspect(options)
---
+++ app/v01/ch15/maps3.exs
@@ -0,0 +1,6 @@
+team = %{
+  leader: %{name: "Charlie", age: 48},
+  subleader: %{name: "Alice", age: 23}
+}
+
+IO.inspect(team.leader.age)
